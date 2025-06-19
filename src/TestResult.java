6f1996e28cd33d813e05b0e84c58da57a7a7b514
xxxx3423 <artem7400qq@gmail.com>
2025-06-19 21:36:23 +0300

    Dodano

--- src/TestResult.java
+++ src/TestResult.java
@@ -1,12 +1,14 @@
-//package test;
-
-//import domain.Exercise;
+import java.time.LocalDate;
 
 public class TestResult {
 
     public static void main(String[] args) {
+        LocalDate startDate = LocalDate.of(2024, 6, 1);
+        LocalDate endDate = LocalDate.of(2024, 6, 14);
 
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        int hoursWorked = Exercise.Calculate(startDate, endDate);
+
+        System.out.println("Employee worked " + hoursWorked + " hours from " +
+                startDate + " to " + endDate + ".");
     }
 }
